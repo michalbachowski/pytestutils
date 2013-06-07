871707153d5d5312621a440c196cf80c6dd29070
Michał Bachowski <michal@bachowski.pl>
2013-06-07 16:43:30 +0200

    Fixed package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(name='PyTestUtils',
     url='https://github.com/michalbachowski/pytestutils',
-    version='0.1.0',
+    version='0.2.2',
     description='Test utilities for my python projects',
     license='New BSD License',
     author='Michał Bachowski',
